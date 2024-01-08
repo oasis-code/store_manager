d632f1fa761d45ecea3cd2f3793c7bec9f170a2e
oasis-code <andindaruth@gmail.com>
2024-01-08 19:18:46 +0300

    cleaned up a few things

---
+++ database/migrations/2024_01_08_090119_create_lubs_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lubs_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('vehicle_id')->nullable();
+            $table->foreign('vehicle_id')->references('id')->on('vehicles');
+            $table->unsignedBigInteger('person_id')->nullable();
+            $table->foreign('person_id')->references('id')->on('people');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('type');
+            $table->decimal('quantity', 15, 2)->default(0.00); // Adjust the precision and scale as needed  
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lubs_transactions');
+    }
+};
